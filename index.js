7d7a888f0c36d189207bad296065372b4bf82d82
felipey2010 <mahamaphilip@yahoo.com>
2020-12-22 23:58:46 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log("Now listening for request on port: " + PORT);
